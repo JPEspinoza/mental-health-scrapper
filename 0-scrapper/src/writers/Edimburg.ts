ec51e9549e7b9820893dfbd8a928a177f50d2cb1
Juan Pablo Espinoza <juanespinoza830@pm.me>
2024-05-06 02:44:47 -0400

    ingress

--- 0-scrapper/src/writers/Edimburg.ts
+++ 0-scrapper/src/writers/Edimburg.ts
@@ -4,45 +4,81 @@ import DeisResults from '../deis/DeisResults';
 import DeisClient from '../deis/DeisClient';
 import { COMUNAS } from '../Constants';
 
-const PAYLOADS = [
-	'EdimburgPostPartum',
-	'EdimburgPregnant',
-	'MCHAT18month',
-	'MCHAT30month',
+interface Payload {
+	// key is name, value is description
+	[key: string]: string;
+}
 
-	// month
-	'ConsultationSpecialistByMonth',
-	'AttendanceByMonth',
-	'ConsultationByMonth',
+const PAYLOADS: Payload = {
+	// Atenciones de salud mental
+	/*
+		Corresponde a la Intervención ambulatoria individual o grupal, realizada por un profesional, técnico y/o gestor comunitario. La intervención incluye consejería, evaluación y confirmación diagnóstica, elaboración de plan de cuidados integrales, psicoeducación, acciones de emergencia y desastres, entre otras prestaciones.
+		Estas atenciones que se describen en este apartado, se analizan desde un punto de vista territorial a nivel nacional, región y Servicio de Salud. Además, se describe los resultados según sexo, grupo de edad y tipo de prestación (profesional/técnico) según año y mes.
+ 		Fuente: REMA 06
+	*/
+	'AttendanceByGender': 'Total Atenciones del Programa de Salud Mental según sexo',
+	'AttendanceByAge': 'Total Atenciones del Programa de Salud Mental por Grupo de edad',
+	'AttendanceByProfessional': 'Distribución Porcentual de Atenciones del Programa de Salud Mental por profesional',
+	'AttendanceByMonth': 'Atenciones del Programa de Salud Mental por mes y Año',
 
-	// age
-	'AttendanceByAge',
-	'ConsultationByAge',
-	'ConsultationSpecialistByAge',
+	// Consultas medicas
+	'ConsultationByGender': 'Total Consultas Médicas del Programa de Salud Mental según sexo',
+	'ConsultationByAge': 'Consultas Médicas del Programa de Salud Mental por Grupo de edad',
+	'ConsultationBySpecialGroup': 'Consultas Médicas del Programa de Salud Mental grupos de especial protección',
+	'ConsultationByMonth': 'Número de Consultas Médicas por mes y Año',
 
-	// gender
-	'AttendanceByGender',
-	'ConsultationByGender',
-	'ConsultationSpecialistByGender',
+	// consultas medicas de especialidad
+	'ConsultationSpecialist': 'Total Atenciones Médicas  Especialidad  Salud Mental',
+	'ConsultationSpecialistByGender': 'Porcentaje de Atenciones Médicas Especialidad Salud Mental por sexo',
+	'ConsultationSpecialistByAge': 'Consultas Médicas Especialidad Salud Mental por Grupo de edad',
+	'ConsultationSpecialistByMonth': 'Consultas Médicas Especialidad Salud Mental por mes',
 
-	// specialist
-	'ConsultationSpecialist',
+	// Evaluaciones de salud mental
+	'EdimburgPregnant': 'Evaluaciones a Gestantes con Escala de Edimburgo según resultado',
+	'EdimburgPostPartum': 'Evaluaciones a Mujeres Post Parto con Escala de Edimburgo según resultado',
+	'MCHAT18month': 'Aplicación Tamizaje Trastorno Espectro Autista (MCHAT) - Niños(as) con Control a los 18 meses según sexo',
+	'MCHAT30month': 'Aplicación Tamizaje Trastorno Espectro Autista (MCHAT) - Evaluación Niños 16 a 30 meses según sexo',
 
-	// professional
-	'AttendanceByProfessional',
+	// Ingresos al programa de salud mental
+	'IngressByGender': 'Total de Ingresos al Programa de Salud Mental según sexo',
+	'IngressByAge': 'Total de Ingresos al Programa de Salud Mental por grupo de edad',
+	'IngressBySpecialGroup': 'Ingresos al Programa de Salud Mental grupos de especial protección',
+	'IngressByMonth': 'Ingresos Programa de Salud Mental por mes y año',
 
-	// special group
-	'ConsultationBySpecialGroup'
+	// Ingresos al programa de salud mental - diagnosticos
 
-];
+	// Egresos del programa de salud mental
+
+	// Egresos del programa de salud mental - diagnosticos
+
+	// Atenciones remotas contexto COVID
+
+	// Intervenciones por patron de consumo
+
+	// Poblacion en control
+
+	// Poblacion en control - diagnosticos
+
+	// Atenciones de urgencia
+
+	// Egresos hospitalarios
+
+	// Egresos hospitalarios por diagnosticos
+
+	// Mortalidad por suicidio
+
+	// Atenciones de urgencia. Analisis comunal
+
+	// Indicador reingreso hospitalario
+};
 
 export default class Edimburg {
 	public static getRequiredPayloads(): string[] {
-		return PAYLOADS;
+		return Object.keys(PAYLOADS);
 	}
 
 	public static write(_context: Context, _client: DeisClient, results: DeisResults): void {
-		for (const payload of PAYLOADS)
+		Object.entries(PAYLOADS).forEach(([payload, description]) =>
 		{
 			for (const commune of COMUNAS) {
 				const comuna = commune.commune;
@@ -83,6 +119,7 @@ export default class Edimburg {
 						'stringTable': stringTable, 
 						'columns': columns,
 						'data': data,
+						'description': description
 					});
 
 					fs.writeFile(`data/${payload}-${comuna}-${establishment}.json`, result_string, function (err: any) {
@@ -90,6 +127,6 @@ export default class Edimburg {
 					});
 				}
 			}
-		}
+		});
 	}
 }
